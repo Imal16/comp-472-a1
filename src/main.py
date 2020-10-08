e7987174cfc8c56dafeba2ca8621040fec8a0e29
OliRac <olivieracette@gmail.com>
2020-10-08 14:17:36 -0400

    Redo using numpy methods
    Except for info_1 and info_1, loadtxt and genfromtxt did not agree with those for some reason...played with params and nothing gave a result that I liked.

--- src/main.py
+++ src/main.py
@@ -2,42 +2,56 @@
 import sklearn
 import csv
 import numpy as np
+import sys
 from pathlib import Path
 
 
-#Kinda ugly but idk if we have a better to do this
 data_folder = Path("dataset/")
-info_1 = data_folder / "info_1.csv"     #info_1 and info_2 are special cases since they contain headers. That row will need to be skipped.
-info_2 = data_folder / "info_2.csv"
-test_no_label_1 = data_folder / "test_no_label_1.csv"
-test_no_label_2 = data_folder / "test_no_label_2.csv"
-test_with_label_1 = data_folder / "test_with_label_1.csv"
-test_with_label_2 = data_folder / "test_with_label_2.csv"
-train_1 = data_folder / "train_1.csv"
-train_2 = data_folder / "train_2.csv"
-val_1 = data_folder / "val_1.csv"
-val_2 = data_folder / "val_2.csv"
 
 
-#Returns a numpy array comprised of the data within the passed csv file
-#Useless now that I found np.genfromtxt() LOL
-def get_data(csv_file):
-    contents = []
+#Shorthand for numpy's loadtxt method.
+#Returns matrices of integers.
+#Assumes the file exists. If not, chaos ensues.
+def get_data(fileName):
+    return np.loadtxt(data_folder / fileName, delimiter=',', dtype='i') 
 
-    with open(csv_file) as file:
-        reader = csv.reader(file)
+
+#numpy's methods to read from files don't seem to work well with the info_x files...
+#Returns dictionary where key is index, value is a character
+def buildSymbolTable(fileName):
+    symbols = {}
+
+    with open(data_folder / fileName) as file:
+        reader = csv.DictReader(file)
 
         for row in reader:
-            contents.append(row)
-
-    return np.array(contents, dtype=np.int32)
+            symbols[int(row['index'])] = row['symbol']
+    
+    return symbols
 
 
 #Runs stuff
 def run():
-    print(get_data(test_no_label_1))
-    print(np.genfromtxt(test_no_label_1, delimiter=',', dtype=np.int32))
+    print("Reading csv data...")
 
+    test_no_label_1 = get_data("test_no_label_1.csv")
+    test_no_label_2 = get_data("test_no_label_2.csv")
+    test_with_label_1 = get_data("test_with_label_1.csv")
+    test_with_label_2 = get_data("test_with_label_2.csv")
+    train_1 = get_data("train_1.csv") 
+    train_2 = get_data("train_2.csv") 
+    val_1 = get_data("val_1.csv") 
+    val_2 = get_data("val_2.csv")
+
+    #info_1 and info_2 are special cases; seems like numpy loadtxt() and genfromtxt() really don't like to deal with those. 
+    #will try out csv reader or dict reader instead.
+    info_1 =  buildSymbolTable("info_1.csv")    
+    info_2 = buildSymbolTable("info_2.csv")
+
+    print("Done!")
+
+
+    #From here, run the specified training algorithm
 
 if __name__ == "__main__":
     run()
